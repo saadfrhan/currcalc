35f464256a0eefcd58582d805c89680ec4cb5c1d
Saad Farhan <saadfarhan347@gmail.com>
2023-01-11 06:46:53 +0000

    Update .env.prod and index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,25 +3,33 @@
 import inquirer from "inquirer";
 import welcome, { doReplay } from "./utils/index.js";
 import { questions } from "./questions.js";
-import inquirerPrompt from 'inquirer-autocomplete-prompt';
+import inquirerPrompt from "inquirer-autocomplete-prompt";
 import chalk from "chalk";
 import { searchForTargetAmount } from "./api/index.js";
 
-inquirer.registerPrompt('autocomplete', inquirerPrompt);
+inquirer.registerPrompt("autocomplete", inquirerPrompt);
 
 async function main() {
-  console.log('invoked')
+  console.log("invoked");
   while (true) {
+    console.log("invoked");
+
     const { from, to, amount } = await inquirer.prompt(questions);
-    const { date, result, rate } = (await searchForTargetAmount({ from, to, amount }))!;
+    const { date, result, rate } = (await searchForTargetAmount({
+      from,
+      to,
+      amount,
+    }))!;
     console.log(result);
-    console.log(amount !== 1 ? rate : '');
+    console.log(amount !== 1 ? rate : "");
     console.log(date);
     if ((await doReplay()).continue) {
       break;
     }
   }
-  return console.log(chalk.green('Thanks for using the Currency Converter CLI!'));
+  return console.log(
+    chalk.green("Thanks for using the Currency Converter CLI!")
+  );
 }
 
 // await welcome();
